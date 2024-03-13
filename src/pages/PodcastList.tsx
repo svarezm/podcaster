e2e14e2dcba95e0ebd1c16fa7fbffd038e884bbd
Sergio Varez <svarez@minsait.com>
2024-03-13 11:25:31 +0100

    feat: [podcaster] creating podcast list component

--- src/pages/PodcastList.tsx
+++ src/pages/PodcastList.tsx
@@ -1,9 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Podcast } from '../types/podcast';
+
 const PodcastList: React.FC = () => {
+    const [podcasts, setPodcasts] = useState<Podcast[]>([]);
+    const [filteredPodcasts, setFilteredPodcasts] = useState<Podcast[]>([]);
+
+    useEffect(() => {
+        const podcasts = [{
+            id: '1234',
+            name: 'name',
+            artist: 'artist',
+            image: 'image',
+        }]
+        setFilteredPodcasts(podcasts);
+    }, []);
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const searchTerm = event.target.value.toLowerCase();
+        const filtered = podcasts.filter(podcast =>
+            podcast.name.toLowerCase().includes(searchTerm)
+        );
+        setFilteredPodcasts(filtered);
+    };
+
     return (
         <div>
-            PodcastList
+            <input type="text" placeholder="Search..." onChange={handleSearch} />
+            <div className="podcast-list">
+                {filteredPodcasts.map((podcast) => (
+                    <div className="podcast-card" key={podcast.id}>
+                        <a href={`/podcast/${podcast.id}`} className="podcast-link">
+                            <div className="podcast-image-container">
+                                <img className="podcast-image" src={podcast.image} alt={`${podcast.name} - ${podcast.artist}`} />
+                            </div>
+                            <div className="podcast-info">
+                                <p className="podcast-title">{podcast.name}</p>
+                                <p className="podcast-artist">{podcast.artist}</p>
+                            </div>
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default PodcastList;
---
+++ src/types/podcast.ts
@@ -0,0 +1,6 @@
+export type Podcast = {
+    id: string;
+    name: string;
+    artist: string;
+    image: any;
+}
