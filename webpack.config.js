587a1ecafd74a4d5a4989d27db997b7ef051c107
Sergio Varez <svarez@minsait.com>
2024-04-01 10:16:56 +0200

    feat: [podcaster] remove unused dependency

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isDevelopment = argv.mode === 'development';
